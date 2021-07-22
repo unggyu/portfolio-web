27ec1e3d77bac835cbb130e2f1e9095c12117b5b
Choi-UngGyu <cdr982116@gmail.com>
2021-07-22 23:46:09 +0900

    Edit title

--- pages/index.tsx
+++ pages/index.tsx
@@ -80,7 +80,7 @@ const IndexPage: NextPage<IndexPageProps> = (props: IndexPageProps) => {
     return (
       <>
         <Head>
-          <title>Unggyu-Choi | Front-end Developer</title>
+          <title>UngGyu-Choi's Portfolio</title>
           <meta charSet="utf-8" />
           <link rel="icon" href="favicon.ico" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
